63faaa4c59181cc6d517629bad2d836d9ef187ea
Civitaspo <civitaspo@gmail.com>
2020-05-01 06:40:46 +0900

    Apply the gradle plugin: embulk-plugins: 0.4.1

--- lib/embulk/filter/copy.rb
+++
@@ -1,8 +0,0 @@
-Embulk::JavaPlugin.register_filter(
-  "copy", "org.embulk.filter.copy.CopyFilterPlugin",
-  File.expand_path('../../../../classpath', __FILE__))
-
-Embulk::JavaPlugin.register_input(
-    "internal_forward",
-    "org.embulk.filter.copy.plugin.InternalForwardInputPlugin",
-    File.expand_path('../../../../classpath', __FILE__))
